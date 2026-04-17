7a2a718f4c56e105c9dfe9d51b4a1130c29b3633
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-04-17 18:43:28 +0300

    news

---
+++ local/templates/.default/components/bitrix/news.list/articles_list/.description.php
@@ -0,0 +1,8 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateDescription = array(
+    "NAME" => "Шаблон списка статей из build",
+    "DESCRIPTION" => "Кастомный шаблон для стажировки, созданный на основе готовой верстки",
+);
+?>
