d3e4b4950c5843560944f918c7296340237c4c70
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-04-27 15:23:14 +0300

    Delete local/templates/.default/components/bitrix/news.list/articles_list directory

--- local/templates/.default/components/bitrix/news.list/articles_list/.description.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$arTemplateDescription = array(
-    "NAME" => "Шаблон списка статей из build",
-    "DESCRIPTION" => "Кастомный шаблон для стажировки, созданный на основе готовой верстки",
-);
-?>
