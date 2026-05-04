e58518fd21b5ffe642edc6e0617847f50cfe1d8b
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-05-04 18:01:19 +0300

    Update detail.php

--- local/templates/.default/components/bitrix/news/.default/detail.php
+++ local/templates/.default/components/bitrix/news/.default/detail.php
@@ -3,7 +3,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-// 1. Вызываем сам компонент детальной новости
 $APPLICATION->IncludeComponent(
     "bitrix:news.detail",
     "",
@@ -14,7 +13,7 @@ $APPLICATION->IncludeComponent(
         "ELEMENT_CODE" => $arResult["VARIABLES"]["ELEMENT_CODE"],
         "FIELD_CODE" => $arParams["DETAIL_FIELD_CODE"],
         "PROPERTY_CODE" => $arParams["DETAIL_PROPERTY_CODE"],
-        "CACHE_TYPE" => "N", // Кэш отключен, чтобы изменения применились сразу
+        "CACHE_TYPE" => "N", // Кэш отключен, чтобы изменения применились сразу, просто из-за конфликта со стилями
         "CACHE_TIME" => $arParams["CACHE_TIME"],
         "DISPLAY_NAME" => "Y",
         "DISPLAY_DATE" => "Y",
@@ -22,7 +21,6 @@ $APPLICATION->IncludeComponent(
     $component
 );
 
-// 2. Жёстко выводим кнопку сразу ПОСЛЕ компонента
 ?>
 <div style="max-width: 1000px; margin: 30px auto 0; padding: 0 20px; box-sizing: border-box; font-family: sans-serif;">
     <a class="article-card__button" href="/nov/" style="text-decoration: none; text-transform: uppercase; color: #333; font-size: 13px; border-bottom: 1px solid #333; padding-bottom: 2px; font-weight: bold; display: inline-block;">
