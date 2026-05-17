9a2221bd74bc180b49499f9ef646ea393e276ab7
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-05-17 19:42:45 +0300

    Update init.php

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,1 +1,16 @@
+<?php
+use Bitrix\Main\Loader;
+
+Loader::registerAutoLoadClasses(
+    "dev.site",
+    [
+        "Dev\\Site\\Agents\\Iblock" => "lib/Agents/Iblock.php",
+    ]
+);
+
+if (Loader::includeModule('dev.site')) {
+    $eventManager = \Bitrix\Main\EventManager::getInstance();
+    $eventManager->addEventHandler('iblock', 'OnAfterIBlockElementAdd', ['Dev\\Site\\Handlers\\Iblock', 'addLog']);
+    $eventManager->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', ['Dev\\Site\\Handlers\\Iblock', 'addLog']);
+}
 
