a001fcefec9a92494666889a8d2cedb7ff764753
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-05-23 12:27:49 +0300

    Add files via upload

---
+++ local/modules/my.complexproperty/lang/ru/include.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS["MY_PROP_IBLOCK_DESC"] = "Комплексное свойство c HTML (cprop + ufhtml)";
+$MESS["MY_PROP_UF_DESC"] = "Комплексное UF-поле с HTML редактором";
+$MESS["MY_PROP_TITLE_LABEL"] = "Заголовок подраздела (Строка):";
+$MESS["MY_PROP_HTML_LABEL"] = "Описание (HTML редактор):";
