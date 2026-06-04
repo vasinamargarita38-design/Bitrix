16fc53a1f2b160374d913045aee2a627cc4c33c3
vasinamargarita38-design <vasinamargarita38@gmail.com>
2026-06-04 15:52:21 +0300

    Delete local/modules/my.complexproperty directory

--- local/modules/my.complexproperty/lang/ru/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$MESS["MY_PROP_IBLOCK_DESC"] = "Комплексное свойство c HTML (cprop + ufhtml)";
-$MESS["MY_PROP_UF_DESC"] = "Комплексное UF-поле с HTML редактором";
-$MESS["MY_PROP_TITLE_LABEL"] = "Заголовок подраздела (Строка):";
-$MESS["MY_PROP_HTML_LABEL"] = "Описание (HTML редактор):";
